94a8df3f1da54a995c4ad8e824753d6bb6886e0b
rasbabbo <rasbabbo@yahoo.com>
2014-07-22 06:35:29 -0700

    all pages up

--- app.js
+++ app.js
@@ -11,42 +11,52 @@ app.set('view engine', 'ejs');
 
 //homepage
 
-app.get('/', function(req, res){
-  res.render('site/index');
-});
+// app.get('/', function(req, res){
+//   res.render('articles/articles', {articles: articles});
+// });
 
 //articles
-app.get('/articles', function(req, res){
+app.get('/articles/new', function(req, res){
 	// res.send('articles')
-  res.render("articles/article", {articles: articles});
+  res.render("articles/new");
 });
 
-app.get('/articles/new', function(req, res) {
-	res.send('new article')
-	// res.render("articles/new");
-});
+// app.get('/articles/new', function(req, res) {
+// 	// res.send('new article')
+// 	res.render("articles/new");
+// });
+
+// app.get('/about', function(req, res) {
+// 	res.send('about')
+// });
+app.get('/contact', function(req, res) {
+	res.render('site/contact');
+})
 
 app.get('/about', function(req, res) {
-	res.send('site/about')
+	res.render('site/about');
 });
 
 app.get('/contact', function(req, res) {
-	res.send('site/contact')
+	res.send('/contact')
+})
+
+app.get('/articles', function(req, res) {
+	res.send(articles);
 })
 
 app.get('/articles/:id', function(req, res) {
 	// res.send("new id")
-
-// 	var index = req.params.id;
-// 	var article = articles[index];
-// 	res.render("articles/show", {article: article})
+	var index = req.params.id;
+	var article = articles[index];
+	res.render("articles/show", {article: article})
 });
 
 app.post('/articles', function(req, res) {
-	console.log(req.body.article);
-	books.push(req.body.article);
-	console.log("thanks dude!")
-	res.redirect('/articles/');
+	console.log(req.body.articles);
+	articles.push(req.body.article);
+	console.log(articles)
+	res.redirect('/articles');
 });
 
 app.listen(3000);
